8d08a459f73af46da011fce21c9c0c60bf105d4e
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-06-01 23:00:59 +0200

    Last commit before the weekend

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Let's get going!</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
